467d4510d3e22f0009792979008ac945aeda4576
Nick Mandel <nrmandel@ucdavis.edu>
2014-12-01 19:28:55 -0800

    resolves jquery error, completes basic color trail functionality

--- color_trail_extension/clickhandle.js
+++ color_trail_extension/clickhandle.js
@@ -1,6 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log('Making ' + tab.url + ' colors!');
-  chrome.tabs.executeScript(null, {
-    file: "trail.js"
-  });
+  chrome.tabs.executeScript(null, 
+  	{file: "jquery.js"},
+  	function() {
+  		chrome.tabs.executeScript(null, {file: "trail.js"});
+  	}
+  );
 });
--- color_trail_extension/trail.js
+++ color_trail_extension/trail.js
@@ -1,17 +1,51 @@
 // document.body.style.backgroundColor="green";
 
-var circle = document.createElement("div");
-circle.setAttribute("id", "circle");
-document.body.appendChild(circle);
-// document.getElementById("circle").style.top = "200px";
-// document.getElementById("circle").style.right = "500px";
-$(document).mousemove(function(event) {
-	// console.log(event.pageX + " " + event.pageY);
-	// console.log($(document).scrollTop());
-	var xPos = event.pageX;
-	var yPos = parseInt(event.pageY) - $(document).scrollTop();
-	var circleElem = document.getElementById("circle")
-	circleElem.style.top = yPos + "px";
-  circleElem.style.left = xPos + "px";
+$(document).ready(function() {
+
+	var numBalls = 8;
+	var colors = [
+	  "#FF0000",
+	  "#FF8000",
+	  "#FFFF00",
+	  "#40FF00",
+	  "#00FFFF",
+	  "#0000FF",
+	  "#8000FF",
+	  "#FF00FF"
+	];
+	var circles = [];
+	var cursorPositions = [];
+
+	for (var i = 0; i < numBalls; i++) {
+		var circle = document.createElement("div");
+		circle.setAttribute("id", "circle" + i);
+		circle.setAttribute("class", "circle")
+		document.body.appendChild(circle);
+		circles.push(circle);
+		cursorPositions.push([0, 0]);
+	};
+
+  var intervalCount = 0;
+	setInterval(function() {
+    var index = intervalCount % numBalls;
+    intervalCount++;
+		cursorPositions[index] = [window.xPos, window.yPos];
+		// console.log(cursorPositions[index]);
+	}, 50)
+
+	$(document).mousemove(function(event) {
+		// console.log(event.pageX + " " + event.pageY);
+		// console.log($(document).scrollTop());
+		window.xPos = parseInt(event.pageX);
+		window.yPos = parseInt(event.pageY) - $(document).scrollTop();
+		for (var a = 0; a < circles.length; a++) {
+			var xCord = cursorPositions[a][0];
+			var yCord = cursorPositions[a][1];
+			var circleElem = document.getElementById("circle" + a)
+			circleElem.style.background = colors[a];
+			circleElem.style.top = (yCord) + "px";
+		  circleElem.style.left = (xCord) + "px";
+		}
+	});
+	
 })
-console.log($("#circle"));
