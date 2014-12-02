061ed252c7752cc8b63b9849117a89daafad50d2
Nick Mandel <nrmandel@ucdavis.edu>
2014-12-01 16:15:04 -0800

    dot now correctly follows cursor when page is scrolled

--- color_trail_extension/trail.js
+++ color_trail_extension/trail.js
@@ -7,10 +7,11 @@ document.body.appendChild(circle);
 // document.getElementById("circle").style.right = "500px";
 $(document).mousemove(function(event) {
 	// console.log(event.pageX + " " + event.pageY);
-	var xPos = window.innerWidth - parseInt(event.pageX);
-	var yPos = event.pageY;
+	// console.log($(document).scrollTop());
+	var xPos = event.pageX;
+	var yPos = parseInt(event.pageY) - $(document).scrollTop();
 	var circleElem = document.getElementById("circle")
-	circleElem.style.top = yPos+ "px";
-  circleElem.style.right = xPos + "px";
+	circleElem.style.top = yPos + "px";
+  circleElem.style.left = xPos + "px";
 })
 console.log($("#circle"));
