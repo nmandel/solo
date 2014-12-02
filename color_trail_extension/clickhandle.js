8efede89c019113acf167621d042f3403fdc97c5
Nick Mandel <nrmandel@ucdavis.edu>
2014-12-02 15:42:53 -0800

    implements d3, explosions, color ring chases cursor

--- color_trail_extension/clickhandle.js
+++ color_trail_extension/clickhandle.js
@@ -1,9 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log('Making ' + tab.url + ' colors!');
-  chrome.tabs.executeScript(null, 
-  	{file: "jquery.js"},
-  	function() {
-  		chrome.tabs.executeScript(null, {file: "trail.js"});
-  	}
-  );
+  chrome.tabs.executeScript(null, {file: "jquery.js"}, function() {
+    chrome.tabs.executeScript(null, {file: "d3.js"}, function() {
+		  chrome.tabs.executeScript(null, {file: "trail.js"});
+		})
+	})
 });
--- color_trail_extension/trail.js
+++ color_trail_extension/trail.js
@@ -13,8 +13,20 @@ $(document).ready(function() {
 	  "#8000FF",
 	  "#FF00FF"
 	];
+	var mag = 200;
+	var directions = [
+    [mag, 0], 
+    [mag, mag],
+    [0, mag],
+    [mag, -mag],
+    [-mag, mag],
+    [-mag, 0],
+    [0, -mag],
+    [-mag, -mag]
+	];
 	var circles = [];
 	var cursorPositions = [];
+	var exploding = false;
 
 	for (var i = 0; i < numBalls; i++) {
 		var circle = document.createElement("div");
@@ -22,30 +34,78 @@ $(document).ready(function() {
 		circle.setAttribute("class", "circle")
 		document.body.appendChild(circle);
 		circles.push(circle);
-		cursorPositions.push([0, 0]);
+		cursorPositions.push([-50, -50]);
 	};
 
   var intervalCount = 0;
 	setInterval(function() {
-    var index = intervalCount % numBalls;
-    intervalCount++;
-		cursorPositions[index] = [window.xPos, window.yPos];
-		// console.log(cursorPositions[index]);
-	}, 50)
+		if (!exploding) {
+	    var index = intervalCount % numBalls;
+	    intervalCount++;
+			cursorPositions[index] = [window.xPos, window.yPos];
+			d3.select("#circle" + index).transition()
+			    .duration(240)
+			    .style("top", (cursorPositions[index][1] + "px"))
+			    .style("left", (cursorPositions[index][0] + "px"));
+			for (var a = 0; a < circles.length; a++) {
+				var xCord = cursorPositions[a][0];
+				var yCord = cursorPositions[a][1];
+				// var circleElem = document.getElementById("circle" + a)
+				// circleElem.style.background = colors[a];
+				var circleElem = "#circle" + a;
+				d3.select(circleElem).style("background", colors[a]);
+				// circleElem.style.top = (yCord) + "px";
+			 //  circleElem.style.left = (xCord) + "px";
+			  // d3.select(circleElem).style("top", (yCord + "px"));
+			  // d3.select(circleElem).style("left", (xCord + "px"));
+			}
+		}
+		// console.log(cursorPositions.toString());
+	}, 30)
+
+  $(document).on('click', function() {
+  	console.log('click!');
+  	exploding = true;
+  	for (var a = 0; a < circles.length; a++) {
+				var circleElem = "#circle" + a;
+				var xCord = parseInt(d3.select(circleElem).style("top"));
+				var yCord = parseInt(d3.select(circleElem).style("left"));
+				// console.log(xCord + " , " + yCord);
+				d3.select(circleElem).transition()
+				  .duration(700)
+				  .style("top", (xCord + directions[a][0]) + "px")
+				  .style("left", (yCord + directions[a][1]) + "px")
+				  .each("end", function() {
+				  	// console.log(this);
+				  	d3.select(this).transition()
+				  	  .duration(700)
+				  	  .style("top", (xCord + "px") )
+				      .style("left", (yCord + "px")	)
+				      .each("end", function() {
+				      	exploding = false;
+				      })
+				  });
+				// d3.select(circleElem).style("background", colors[a]);
+			 //  d3.select(circleElem).style("top", (yCord + "px"));
+			 //  d3.select(circleElem).style("left", (xCord + "px"));
+			}
+  	// console.log(cursorPositions.toString());
+    
+  })
+
+  $(document).on('mouseup', function() {
+  	if (exploding) {
+	  	console.log('mouseup!');
+	  	exploding = false;
+  	}
+  })
 
 	$(document).mousemove(function(event) {
 		// console.log(event.pageX + " " + event.pageY);
 		// console.log($(document).scrollTop());
 		window.xPos = parseInt(event.pageX);
 		window.yPos = parseInt(event.pageY) - $(document).scrollTop();
-		for (var a = 0; a < circles.length; a++) {
-			var xCord = cursorPositions[a][0];
-			var yCord = cursorPositions[a][1];
-			var circleElem = document.getElementById("circle" + a)
-			circleElem.style.background = colors[a];
-			circleElem.style.top = (yCord) + "px";
-		  circleElem.style.left = (xCord) + "px";
-		}
+		
 	});
 	
 })
